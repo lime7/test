7f76056220639b24e09c50da9a0e7b18368b345b
lime7 <senseless.v.7@gmail.com>
2021-08-03 16:21:33 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict'
 
 console.log('S>>>')
+// eslint-disable-next-line import/first
+import Collapse from 'bootstrap/js/dist/collapse'
+// eslint-disable-next-line import/first
+import Dropdown from 'bootstrap/js/dist/dropdown'
+// eslint-disable-next-line import/first
+import Modal from 'bootstrap/js/dist/modal'
+// eslint-disable-next-line import/first
+import Button from 'bootstrap/js/dist/button'
+// eslint-disable-next-line import/first
+import Offcanvas from 'bootstrap/js/dist/offcanvas'
